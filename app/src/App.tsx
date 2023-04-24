5a9735adbddf8e88375adc5242cdbd1047031e84
medricet <medricet@gmail.com>
2023-04-24 12:29:51 -0400

    added some type information for api, refactored search options

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,30 @@
 import { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 import Chart from './comp/Chart'
-import OrganizeBy from './comp/OrganizeBy'
+import SearchOptions from './comp/SearchOptions'
 import Slider from './comp/Slider'
 
 import './App.css'
 
-export default function App() {
-  const [artistInput, setArtistInput] = useState<string>()
+type Track = {
+  features: Features,
+}
 
-  const onArtistFind = () => {
-    console.log('Find Artist')
-  }
+type Features = {
+  acousticness: number,
+  danceability: number,
+  duration_ms: number,
+  energy: number,
+  instrumentalness: number,
+  liveness: number,
+  loudness: number,
+  speechiness: number,
+  tempo: number,
+  valence: number,
+}
+
+export default function App() {
+  const [tracks, setTracks] = useState<Array<Track> | null>(null)
   
   return (
     <div className="w-screen h-screen bg-spotify-dark-gray">
@@ -21,27 +32,14 @@ export default function App() {
         <p className="m-6 text-2xl text-white font-bold font-sans">Spotify Ranker</p>
       </div>
       {/* main area */}
-      <div className="flex flex-col items-center my-4 space-y-4">
-        <div className="flex flex-row items-center space-x-2">
-          <FontAwesomeIcon icon={faMagnifyingGlass} className="h-6 text-spotify-green"/>
-          <input
-            type="text"
-            value={artistInput}
-            onChange={ e => setArtistInput(e.target.value) }
-            placeholder="Enter artist name..."
-            className="p-2 bg-spotify-gray rounded-lg text-white"
-          />
-          <button
-            onClick={onArtistFind}
-            className="p-2 rounded-lg bg-spotify-green text-white font-bold"
-          >
-            Find
-          </button>
-        </div>
-
-        <OrganizeBy organizeSettings={{}}/>
-        <Chart items={Array.from({length: 75}, () => Math.random() * 8)}/>
-        <Slider count={75}/>
+      <div className="my-4">
+        <SearchOptions organizeSettings={{}}/>
+        { tracks !== null ? (
+          <div className="flex flex-col items-center my-4 space-y-4">
+            <Chart items={Array.from({length: 500}, () => Math.random() * 8)}/>
+            <Slider count={500}/>
+          </div>
+        ) : <div></div> }
       </div>
     </div>
   )
